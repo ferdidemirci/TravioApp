265c95c66b7e802dc95bd924505b2f5b59d70299
mahmutgazidogan <m.gazi.dogann>
2023-08-31 22:00:31 +0300

    settings model, viewmodel, collectionviewcell added

--- Travio/Views/TabBar/Settings/SettingsVC.swift
+++ Travio/Views/TabBar/Settings/SettingsVC.swift
@@ -8,70 +8,154 @@
 import UIKit
 import SnapKit
 
-class CustomEditView: UIView {
+class SettingsVC: UIViewController {
     
-    var labelText: String = "" {
-        didSet {
-            titleLabel.text = labelText
-        }
-    }
-    
-    var iconImage: String = "" {
-        didSet {
-            iconImageView.image = UIImage(named: iconImage)
-        }
-    }
-    
-    private lazy var iconImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: iconImage)
-        iv.backgroundColor = .clear
-        return iv
-    }()
+    let viewModel = SettingsViewModel()
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: AppFont.medium.rawValue, size: 14)
-        label.text = labelText
+        label.text = "Settings"
+        label.textColor = .white
+        label.font = UIFont(name: AppFont.semiBold.rawValue, size: 32)
         return label
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
+    private lazy var mainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = AppColor.backgroundColor.colorValue()
+        return view
+    }()
+    
+    private lazy var imageViewProfile: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(named: "bruce")
+        iv.backgroundColor = .red
+        iv.layer.masksToBounds = true
+        iv.layer.cornerRadius = 60
+        return iv
+    }()
+    
+    private lazy var lblName: UILabel = {
+        let label = UILabel()
+        label.text = "Bruce Wills"
+        label.textColor = AppColor.secondaryColor.colorValue()
+        label.font = UIFont(name: AppFont.semiBold.rawValue, size: 16)
+        return label
+    }()
+
+    private lazy var btnEditProfile: UIButton = {
+        let button = UIButton()
+        button.setTitle("Edit Profile", for: .normal)
+        button.titleLabel?.font = UIFont(name: AppFont.regular.rawValue, size: 12)
+        button.setTitleColor(AppColor.primaryColor.colorValue(), for: .normal)
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(didTapEditProfileButton), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        collectionView.isPagingEnabled = true
+        collectionView.register(SettingsCollectionViewCell.self, forCellWithReuseIdentifier: SettingsCollectionViewCell().identifier)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-      
         setupViews()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    override func viewDidLayoutSubviews() {
+        mainView.roundCorners(corners: [.topLeft], radius: 80)
+//        imageViewProfile.roundCorners(corners: [.topLeft], radius: 60)
+        
     }
     
-    override func layoutSubviews() {
-        self.roundCorners(corners: [.allCorners], radius: 16)
+    @objc private func didTapEditProfileButton() {
+        print("edit profile button tapped")
     }
     
     private func setupViews() {
-        self.backgroundColor = .white
-        self.addSubviews(iconImageView,
-                         titleLabel)
-        self.addShadow()
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = AppColor.primaryColor.colorValue()
+        self.view.addSubviews(titleLabel,
+                              mainView)
+        self.mainView.addSubviews(imageViewProfile,
+                                  lblName,
+                                  btnEditProfile,
+                                  collectionView)
         
         setupLayouts()
     }
     
     private func setupLayouts() {
-        iconImageView.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(16)
-            make.centerY.equalToSuperview()
-            make.height.width.equalTo(20)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalToSuperview().offset(20)
         }
         
-        titleLabel.snp.makeConstraints { make in
-            make.leading.equalTo(iconImageView.snp.trailing).offset(8)
-            make.centerY.equalToSuperview()
+        mainView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(54)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
+        
+        imageViewProfile.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(24)
+            make.centerX.equalToSuperview()
+            make.height.width.equalTo(120)
+        }
+        
+        lblName.snp.makeConstraints { make in
+            make.top.equalTo(imageViewProfile.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(24)
+        }
+        
+        btnEditProfile.snp.makeConstraints { make in
+            make.top.equalTo(lblName.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(62)
+            make.height.equalTo(18)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(btnEditProfile.snp.bottom).offset(24)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview()
+        }
+        
     }
-    
+
 }
 
+
+extension SettingsVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width: collectionView.frame.width, height: 54)
+        return size
+    }
+}
+
+extension SettingsVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.settingsParameters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SettingsCollectionViewCell().identifier, for: indexPath) as? SettingsCollectionViewCell else { return UICollectionViewCell() }
+        cell.configure(model: viewModel.settingsParameters[indexPath.item])
+//        cell.roundCorners(corners: [.topLeft, .topRight, .bottomLeft], radius: 16)
+        return cell
+    }
+    
+    
+}
