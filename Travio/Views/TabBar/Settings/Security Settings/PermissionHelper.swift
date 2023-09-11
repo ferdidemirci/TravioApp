7c33af1afa05ca304c0014ab75b1e1a41c64247b
mahmutgazidogan <m.gazi.dogann>
2023-09-11 20:26:18 +0300

    comment lines deleted

--- Travio/Views/TabBar/Settings/Security Settings/PermissionHelper.swift
+++ Travio/Views/TabBar/Settings/Security Settings/PermissionHelper.swift
@@ -11,9 +11,9 @@ import Photos
 import CoreLocation
 
 class PermissionHelper {
+    
     let shared = PermissionHelper()
     
-    // Kamera izni kontrolü ve talebi
     static func requestCameraPermission(completion: @escaping (Bool) -> Void) {
         AVCaptureDevice.requestAccess(for: .video) { granted in
             DispatchQueue.main.async {
@@ -22,7 +22,6 @@ class PermissionHelper {
         }
     }
     
-    // Fotoğraf Kütüphanesi izni kontrolü ve talebi
     static func requestPhotoLibraryPermission(completion: @escaping (Bool) -> Void) {
         PHPhotoLibrary.requestAuthorization { status in
             DispatchQueue.main.async {
@@ -32,7 +31,6 @@ class PermissionHelper {
         }
     }
     
-    // Konum izni kontrolü ve talebi
     static func requestLocationPermission(completion: @escaping (Bool) -> Void) {
         let locationManager = CLLocationManager()
         if CLLocationManager.locationServicesEnabled() {
@@ -43,7 +41,6 @@ class PermissionHelper {
                 completion(false)
             case .notDetermined:
                 locationManager.requestWhenInUseAuthorization()
-                // Bu kısımda locationManager.delegate ile izin sonucunu dinleyebilirsiniz.
             @unknown default:
                 completion(false)
             }
@@ -51,6 +48,5 @@ class PermissionHelper {
             completion(false)
         }
     }
-    
     
 }
